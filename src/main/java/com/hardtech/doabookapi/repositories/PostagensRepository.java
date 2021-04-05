b5f0b8aab61dd18c0499f3ea6f0f87f693607a98
Milton-668 <miltonmjcp@gmail.com>
2021-04-05 15:30:27 -0300

    fix(repository): Removido o metodo findByTitulo do repository postagens #14

--- src/main/java/com/hardtech/doabookapi/repositories/PostagensRepository.java
+++ src/main/java/com/hardtech/doabookapi/repositories/PostagensRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.hardtech.doabookapi.models.Postagens;
 
 public interface PostagensRepository extends JpaRepository<Postagens, Long> {
-	public List<Postagens> findAllByTituloContainingIgnoreCase(String titulo);
+	//public List<Postagens> findAllByTituloContainingIgnoreCase(String titulo);
 
 	public List<Postagens> findAllByTextoContainingIgnoreCase(String texto);
 }
